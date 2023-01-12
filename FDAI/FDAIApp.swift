4c20625d3964754dc1c7308f8b470529f01acfc7
Jim Hillhouse <portablefrontier@me.com>
2023-01-11 23:42:34 -0600

    Initial Commit

---
+++ FDAI/FDAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDAIApp.swift
+//  FDAI
+//
+//  Created by James Hillhouse IV on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FDAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
